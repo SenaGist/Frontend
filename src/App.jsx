f10b2db5bec07a073ccc4828b713d864034dddd5
emmanuellopezsalda <emmanuellopezsalda123@gmail.com>
2025-04-09 10:11:26 -0500

    Login conectado con api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
+import { AuthProvider } from './context/AuthContext'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './pages/Login'
 
 function App() {
 
   return (
-    <Login/>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+      </Router>
+    </AuthProvider>
   )
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,26 +1,58 @@
 import { useState } from "react"
+import { useAuth } from "../context/AuthContext";
 
 function Login() {
+    const API_URL = import.meta.env.VITE_API_URL;
+    const [form, setForm] = useState({email: "", password: ""});
+    const { login } = useAuth();
     const [showPassword, setShowPassword] = useState(false);
+
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch(`${API_URL}/auth/login`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(form)
+            });
+    
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+    
+            const data = await response.json();
+            login(data.token);
+            
+        } catch (error) {
+            console.error("Error en el login:", error);
+        }
+    };
 
     const togglePassword = () => {
         setShowPassword(!showPassword);
     }
     return(
         <div className="login-container">
-            <form className="login-form">
+            <form className="login-form" onSubmit={handleSubmit}>
                 <div className="logo">
                     <div className="logo-circle"></div>
                     <div className="logo-title">SENAGIST</div>
                 </div>
                 <div className="email-wrapper">
                     <label>Correo</label>
-                    <input type="email" placeholder="Ingrese su correo" />
+                    <input name="email" type="email" placeholder="Ingrese su correo" onChange={handleChange} />
                 </div>
                 <div className="password-wrapper">
                     <label>Contraseña</label>
-                    <input type={showPassword ? "text" : "password"} placeholder="Ingrese su contraseña" id="password" />
-
+                    <input type={showPassword ? "text" : "password"} 
+                        placeholder="Ingrese su contraseña" 
+                        id="password" onChange={handleChange} name="password"/>
                     <button className="toggle-password" 
                         type="button"
                         onClick={togglePassword}>
